095955a7828648790bd79cd3a257885b01916e32
unhealthyhuman <136725723+unhealthyhuman@users.noreply.github.com>
2024-02-02 16:00:49 +0100

    made Makefile and header file

---
+++ philosophers/philosophers.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ischmutz <ischmutz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 15:56:58 by ischmutz          #+#    #+#             */
+/*   Updated: 2024/02/02 15:59:56 by ischmutz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+
+
+#endif
